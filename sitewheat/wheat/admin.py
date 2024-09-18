48520c16a43682d24532cb9c7ab38946cf7b5294
liset29 <79374931315t@gmail.com>
2024-09-18 14:48:28 +0500

    added models to the database

--- sitewheat/wheat/admin.py
+++ sitewheat/wheat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products,Orders
+
+admin.site.register(Products)
+admin.site.register(Orders)
--- sitewheat/wheat/models.py
+++ sitewheat/wheat/models.py
@@ -5,11 +5,50 @@ class Products(models.Model):
     product_name = models.CharField(max_length=200)
     unit_price = models.FloatField(default=0, blank=False)
     quantity = models.IntegerField(default=0,blank=False)
+    weight = models.FloatField(default=0, blank=False)
 
 
-# class Users(models.Model):
-#     contact_name = models.CharField(max_length=100,blank = False)
-#     city = models.CharField(max_length=100,blank = False)
-#     address = models.CharField(max_length=100,blank = False)
-#     phone = models.CharField(max_length=11,blank= False)
-#
+
+class Users(models.Model):
+    contact_name = models.CharField(max_length=100,blank = False)
+    city = models.CharField(max_length=100,blank = False)
+    address = models.CharField(max_length=100,blank = False)
+    phone = models.CharField(max_length=11,blank= False)
+
+
+class Orders(models.Model):
+    ORDER_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('canceled', 'Canceled'),
+    ]
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    order_data = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    weight = models.FloatField(default=0, blank=False)
+    city = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=100, blank=False)
+    total_price = models.FloatField(default=0, blank=False)
+    status = models.CharField(
+        max_length=20,
+        choices=ORDER_STATUS_CHOICES,
+        default='pending',
+        blank=False
+    )
+    products = models.JSONField()
+
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    products = models.JSONField()
+
+
+
+
+
+
+
+
+
